623715e6294efa2e5e458843411cd8ee794900a0
Pavko <ratalewski@gmail.com>
2020-01-13 21:28:12 +0100

    change number

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -67,7 +67,7 @@ class GridTemplate extends Component {
             <StyledHeading big as="h1">
               {pageContext}
             </StyledHeading>
-            <StyledParagraph>99 {pageContext}</StyledParagraph>
+            <StyledParagraph>100 {pageContext}</StyledParagraph>
           </StyledPageHeader>
           <StyledGrid>{children}</StyledGrid>
           <StyledButtonIcon
